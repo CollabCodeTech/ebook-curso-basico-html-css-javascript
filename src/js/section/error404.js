e7950a0a01e156bc557e974ad38ac81672045464
Marco Bruno <marco.bruno.br@gmail.com>
2019-12-05 19:32:48 +0000

    Evento no play para o game

---
+++ src/js/section/error404.js
@@ -0,0 +1,8 @@
+const $error404 = document.querySelector('.-error404');
+const $game = document.querySelector('.-game');
+const $play = $error404.querySelector('.primary-button');
+
+$play.addEventListener('click', function() {
+  $error404.classList.remove('-active');
+  $game.classList.add('-active');
+});
